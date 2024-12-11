81e1aaae6f90da899b42ad10df2cfe34d0f03265
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-12-11 12:46:55 +0530

    woking on bookRegister page

--- src/main/java/com/bookStore/controller/bookController.java
+++ src/main/java/com/bookStore/controller/bookController.java
@@ -1,11 +1,14 @@
 package com.bookStore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bookStore.entities.Book;
 import com.bookStore.service.BookService;
@@ -32,14 +35,18 @@ public class bookController {
     }
 
     @GetMapping("/available-books")
-    public String getAllBook(@ModelAttribute Book book, BindingResult result) {
-        if (result.hasErrors()) {
-            // Handle validation errors (e.g., return to form)
-            System.out.println("available-books"+result);
-            return "errorPage";
-        }
-        return "bookList";
+public ModelAndView getAllBook(@ModelAttribute Book book, BindingResult result) {
+    if (result.hasErrors()) {
+        // Log validation errors and return to an error page
+        System.out.println("Validation errors: " + result);
+        return new ModelAndView("errorPage");
     }
+    // Fetch the list of books and return it in the ModelAndView
+    List<Book> list = service.getAllBook();
+    
+    return new ModelAndView("bookList", "list", list);  
+}
+
 
     @PostMapping("/save")
     public String addBook(@ModelAttribute Book b, BindingResult result) {
